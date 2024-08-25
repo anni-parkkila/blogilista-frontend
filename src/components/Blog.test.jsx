03abe50057140b9629f36086784729a527a1a2d7
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2024-08-25 18:33:56 +0300

    Step2 nappi ja detaljit

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -3,10 +3,8 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog /> component', () => {
-  let blog
-
   beforeEach(() => {
-    blog = {
+    const blog = {
       title: 'Adventures of Sherlock Holmes',
       author: 'John H. Watson',
       url: 'b221.co.uk',
@@ -30,5 +28,20 @@ describe('<Blog /> component', () => {
 
     const url = screen.queryByText('b221.co.uk')
     expect(url).toBeNull()
+  })
+
+  test('details are shown when "view" button is pressed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const url = screen.getByText('b221.co.uk', { exact: false })
+    expect(url).toBeDefined()
+
+    const likes = screen.getByText('Likes: 7', { exact: false })
+    expect(likes).toBeDefined()
+
+    const name = screen.getByText('SuperUser', { exact: false })
+    expect(name).toBeDefined()
   })
 })
